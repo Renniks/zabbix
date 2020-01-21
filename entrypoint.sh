83ef303bca98c8336151304c102e75be59d02583
Ivan <41379826+Renniks@users.noreply.github.com>
2020-01-21 21:36:13 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# restarting services
+echo "==> Starting Services..."
+/etc/init.d/apache2 stop
+/etc/init.d/php7.2-fpm restart
+/etc/init.d/mysql restart
+/etc/init.d/nginx restart
+/etc/init.d/zabbix-server restart
+/etc/init.d/zabbix-agent start
+
+
+# tail logs
+echo "==> Tailing logs..."
+tail -f \
+    /var/log/zabbix/*.log \
+    /var/log/mysql/error.log \
+    tail /var/log/nginx/error.log
