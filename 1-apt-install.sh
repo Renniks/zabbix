76abe8e4f69ea4a6621f9cfc0abe9672b12f9a52
Ivan <41379826+Renniks@users.noreply.github.com>
2020-01-21 22:14:05 +0300

    Update 1-apt-install.sh
    formatting updated

--- 1-apt-install.sh
+++ 1-apt-install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 echo "Running package installation"
+
 apt update -y && apt install wget -y
 wget https://repo.zabbix.com/zabbix/4.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.2-2+bionic_all.deb
 dpkg -i zabbix-release_4.2-2+bionic_all.deb
 apt update
-apt install -y /
-  mariadb-server /
-  mariadb-client /
-  zabbix-server-mysql /
-  nginx /
-  zabbix-frontend-php /
-  zabbix-agent /
-  php-fpm /
-  php-mysql
+apt install -y mariadb-server mariadb-client zabbix-server-mysql nginx zabbix-frontend-php zabbix-agent php-fpm php-mysql
 
 echo "package installation complete"
