386a906a698d0aaa2d365ab74a01a0c88994c27f
Ivan <41379826+Renniks@users.noreply.github.com>
2020-01-21 21:20:00 +0300

    Update 2-php-conf.sh

--- 2-php-conf.sh
+++ 2-php-conf.sh
@@ -9,5 +9,3 @@ sed -i "s/^\; max_input_vars = 1000/max_input_vars = 10000/" /etc/php/7.2/fpm/ph
 sed -i 's/^\;opcache.enable=1/opcache.enable=0/' /etc/php/7.2/fpm/php.ini
 echo "PHP configuration complete"
 
-# !!! Temp. Setup zabbix server DB Password
-sed -i '124 s/# DBPassword=/DBPassword=zabbix/' /etc/zabbix/zabbix_server.conf
