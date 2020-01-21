c5433f469d44a3518c1a1761b2b9b58a7b53f561
Ivan <41379826+Renniks@users.noreply.github.com>
2020-01-22 00:06:48 +0300

    Update 3-db-init.sh

--- 3-db-init.sh
+++ 3-db-init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+/etc/init.d/mysql restart
+sleep 5
+
+echo "Creating database zabbix"
 mysql -v <<SQL
 CREATE DATABASE zabbix CHARACTER SET UTF8 COLLATE UTF8_BIN;
 CREATE USER zabbix@localhost IDENTIFIED BY 'zabbix';
@@ -7,6 +11,7 @@ GRANT ALL PRIVILEGES ON zabbix.* TO zabbix@localhost;
 FLUSH PRIVILEGES;
 SQL
 
+echo "Applying scheme"
 zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -D zabbix
 
 #setup zabbix server DB Password
