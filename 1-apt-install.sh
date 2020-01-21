075fa2edae48861f8fb14193881944de51265120
Ivan <41379826+Renniks@users.noreply.github.com>
2020-01-21 22:38:33 +0300

    Update 1-apt-install.sh
    Fixing missing schema: /usr/share/doc/zabbix-server-mysql/create.sql.gz

--- 1-apt-install.sh
+++ 1-apt-install.sh
@@ -5,6 +5,7 @@ echo "Running package installation"
 apt update -y && apt install wget -y
 wget https://repo.zabbix.com/zabbix/4.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.2-2+bionic_all.deb
 dpkg -i zabbix-release_4.2-2+bionic_all.deb
+dpkg --path-include='/usr/share/doc/*/*' -i zabbix-server-mysql*
 apt update
 apt install -y mariadb-server mariadb-client zabbix-server-mysql nginx zabbix-frontend-php zabbix-agent php-fpm php-mysql
 
