190386cbc0ab335bf6e5479dc8d080474bfd08fc
Renniks <@>
2020-01-22 14:53:35 +0300

    DB Usr password VAR added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Database user password can be changed here
+DBPassword="somesequrep4ssw0rd"
+
 # Setup zabbix pre-requisites for php.ini
 echo "Starting PHP configuration"
 sed -i 's/^post_max_size.*/post_max_size=16M/' /etc/php/7.2/fpm/php.ini
@@ -10,14 +13,10 @@ sed -i "s/^\; max_input_vars = 1000/max_input_vars = 10000/" /etc/php/7.2/fpm/ph
 sed -i 's/^\;opcache.enable=1/opcache.enable=0/' /etc/php/7.2/fpm/php.ini
 echo "PHP configuration complete"
 
-# Make sure database is up and running
-/etc/init.d/mysql restart
-sleep 5
-
 echo "Creating database zabbix"
 mysql -v <<SQL
 CREATE DATABASE zabbix CHARACTER SET UTF8 COLLATE UTF8_BIN;
-CREATE USER zabbix@localhost IDENTIFIED BY 'zabbix';
+CREATE USER zabbix@localhost IDENTIFIED BY 'DBPassword';
 GRANT ALL PRIVILEGES ON zabbix.* TO zabbix@localhost;
 FLUSH PRIVILEGES;
 SQL
@@ -26,20 +25,20 @@ echo "Applying database schema"
 zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -D zabbix
 
 # Setup zabbix server DB Password
-sed -i '124 s/# DBPassword=/DBPassword=zabbix/' /etc/zabbix/zabbix_server.conf
+sed -i "124 s/# DBPassword=/DBPassword=$DBPassword/" /etc/zabbix/zabbix_server.conf
 
 # Setup nginx configuration
 cat ./nginx_config > /etc/nginx/sites-available/default
 
-# starting services
+# Start services
 echo "Starting Services"
 /etc/init.d/php7.2-fpm restart
 /etc/init.d/mysql restart
 /etc/init.d/nginx restart
+/etc/init.d/zabbix-agent restart
 /etc/init.d/zabbix-server restart
 
-
-# tail logs
+# Tail logs
 echo "Tailing logs"
 tail -f \
     /var/log/zabbix/*.log \
